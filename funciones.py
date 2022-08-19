f7ea8da9d3f9933af9e659b07e5f2b284ece840f
Pablo Geffner <geffnerpablo@gmail.com>
2022-08-19 18:51:56 -0300

    funciones

---
+++ funciones.py
@@ -0,0 +1,29 @@
+# def ingresovalido():
+#     print("gracias por entrar") 
+#     def ingresonovalido():
+#         print("verifique sus datos") 
+    
+# user = "root"
+# pwd  = "pass"
+
+# if user == "root" and pwd == "pass":
+#     ingresovalido()
+# else:
+#     ingresonovalido()
+
+
+def suma (a, b):
+    print ("El resultado es", a + b)
+
+suma ("8", "8")
+
+
+
+
+
+
+
+
+
+
+
